e9c7af96f6fe0ef1372f3e43c2f45ad1f99bb520
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-09-13 14:02:54 -0600

    Up

--- config/prod.exs
+++ config/prod.exs
@@ -3,7 +3,7 @@ import Config
 # Configuración de producción
 config :open_chat, OpenChatWeb.Endpoint,
   http: [port: System.get_env("PORT") || 4000],
-  url: [host: "example.com", port: 443],
+  url: [host: "chat-swinger.nonychat.online", port: 443],
   secret_key_base: System.get_env("SECRET_KEY_BASE"),
   cache_static_manifest: "priv/static/cache_manifest.json"
 
