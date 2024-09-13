ea47567de1a7e89c9c0739e85d6f12bc5c1bcb00
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-09-13 13:56:58 -0600

    Update Prod Mode

--- config/prod.exs
+++ config/prod.exs
@@ -1,13 +1,17 @@
 import Config
 
+# Configuración de producción
 config :open_chat, OpenChatWeb.Endpoint,
-  cache_static_manifest: "priv/static/cache_manifest.json",
-  server: true,
-  root: ".",
-  version: Application.spec(:open_chat, :vsn)
+  http: [port: System.get_env("PORT") || 4000],
+  url: [host: "example.com", port: 443],
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
+  cache_static_manifest: "priv/static/cache_manifest.json"
 
+# Configuración de Swoosh para producción
 config :swoosh, api_client: Swoosh.ApiClient.Finch, finch_name: OpenChat.Finch
 
-config :swoosh, local: false
-
+# Configuración de logs: nivel de logs en producción
 config :logger, level: :info
+
+# Inicializar plugs en tiempo de compilación para mejor rendimiento en producción
+config :phoenix, :plug_init_mode, :compile
