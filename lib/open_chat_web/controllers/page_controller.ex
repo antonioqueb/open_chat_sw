593715fbfb83fdaa8c6eacffb31ba835b599f3f1
Antonio Queb <92013606+antonioqueb@users.noreply.github.com>
2024-09-13 19:13:12 -0600

    Update...

--- lib/open_chat_web/controllers/page_controller.ex
+++ lib/open_chat_web/controllers/page_controller.ex
@@ -2,12 +2,24 @@ defmodule OpenChatWeb.PageController do
   use OpenChatWeb, :controller
 
   def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home, layout: false)
+    conn
+    |> assign(:page_title, "Bienvenido a Swinger Chat México")
+    |> assign(:meta_description, "Descripción del chat más interactivo")
+    |> render(:home, layout: {OpenChatWeb.Layouts, :root})
   end
 
   def terms(conn, _params) do
-    render(conn, :terms, layout: false)
+    conn
+    |> assign(:page_title, "Términos y Condiciones")
+    |> assign(:meta_description, "Lee nuestros términos y condiciones para el uso del servicio.")
+    |> render(:terms, layout: {OpenChatWeb.Layouts, :root})
+  end
+
+  # Nueva acción para manejar la sala de chat por estado
+  def chat_room(conn, %{"state" => state}) do
+    conn
+    |> assign(:page_title, "#{state} Swinger Chat")
+    |> assign(:meta_description, "Chat anónimo para parejas en #{state}")
+    |> render(:chat_room, layout: {OpenChatWeb.Layouts, :root})
   end
 end
